222fdc4ea2950b8781408898b867d250a0e44315
ida.olsson.1361@student.lu.se <90462101+ida02olsson@users.noreply.github.com>
2024-11-17 15:17:47 +0100

    textSize fungerar

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ function startProgram() {
     var textInput = document.getElementById('textarea').value;
     const newContent = document.createTextNode(textInput);
 
+    const getTextSize = document.querySelector('.input').value || 14; //14 eftersom vi har det som default
+    const applyTextSize = `font-size: ${getTextSize}px;`;
+
+
     if (clickHighlightWords && !clickSlidingWords) {
 
         console.log("marked function clicked");
@@ -12,6 +16,9 @@ function startProgram() {
         highlightWordReader.id = "highlightWordReader";
         clearReader("highlightWordReader");
         highlightWordReader.append(newContent);
+
+        highlightWordReader.style = applyTextSize;
+
         const currentDiv = document.getElementById("markReader");
         document.body.insertBefore(highlightWordReader, currentDiv);
         highlightWords(highlightWordReader);
@@ -22,6 +29,9 @@ function startProgram() {
         slidingWordReader.id = "slidingWordReader";
         clearReader("slidingWordReader");
         slidingWordReader.append(newContent);
+
+        slidingWordReader.style = applyTextSize;
+
         const currentDiv = document.getElementById("slidingWordReader"); // funkar ej för tillfället
         document.body.insertBefore(slidingWordReader, currentDiv); // funkar ej för tillfället
 
