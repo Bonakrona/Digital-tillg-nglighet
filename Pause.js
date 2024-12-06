21ba48cf236b4d8b9e3c07a5cb4db4771da2d545
ida.olsson.1361@student.lu.se <90462101+ida02olsson@users.noreply.github.com>
2024-12-06 17:03:13 +0100

    fixade ordningen på pause/resume

--- Pause.js
+++ Pause.js
@@ -6,10 +6,10 @@ pauseButton.addEventListener("click", pauseProgram);
 function pauseProgram() {
     if (paused) {
         paused = false;
-        pauseButton.innerHTML = `<img src="Images/pause1.png" height="22" style="vertical-align: -3px;" /> Resume`;
+        pauseButton.innerHTML = `<img src="Images/pause1.png" height="22" style="vertical-align: -3px;" /> Pause`;
     } else {
         paused = true;
-        pauseButton.innerHTML = `<img src="Images/pause1.png" height="22" style="vertical-align: -3px;" /> Pause`;
+        pauseButton.innerHTML = `<img src="Images/pause1.png" height="22" style="vertical-align: -3px;" /> Resume`;
     }
     
 }
